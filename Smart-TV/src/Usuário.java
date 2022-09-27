1728db514aad8d585cf6c00103639e79b3e856c9
diegofs35 <87245589+diegofs35@users.noreply.github.com>
2022-09-26 21:46:20 -0300

    continuidade da aula

---
+++ Smart-TV/src/Usuário.java
@@ -0,0 +1,35 @@
+public class Usuário {
+    public static void main(String[] args) throws Exception {
+    
+        SmartTV smartTV = new SmartTV();
+    
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+
+        System.out.println("Canal: " + smartTV.canal);
+
+        smartTV.mudarCanal(13);
+
+        System.out.println("Canal atual: " + smartTV.canal);
+        
+        System.out.println("Volume atual: " + smartTV.volume);
+
+        System.out.println("TV ligada? " + smartTV.ligada);    
+        
+        System.out.println("Volume: " + smartTV.volume);
+    
+            smartTV.ligar();
+            System.out.println("Novo status - TV ligada? " + smartTV.ligada);  
+    
+            smartTV.desligar();
+            System.out.println("Novo status - TV ligada? " + smartTV.ligada);
+
+    
+
+
+    }
+
+
+}
